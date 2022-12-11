464fddbe3e76128e72fabc2206dbd878b74d344c
Samira Souza <sfcds.snf22@uea.edu.br>
2022-12-11 14:34:52 -0400

    finish commit

--- turtle/Triangle Turtle.py
+++ turtle/Triangle Turtle.py
@@ -1,14 +1,33 @@
 import turtle
 
-t = turtle.Turtle()
-s = turtle.getscreen()
+# Metodo para obter a tela
+screen = turtle.getscreen()
 
-def triangle():
-    t.fd(100)
-    t.lt(120)
-    t.fd(100)
+# Cria a caneta
+pen = turtle.Turtle()
 
+
+def triangle(x, y):
+
+    # Usado para desenhar sem caneta
+    pen.penup()
+
+    # Muda o cursor ate a posicao x e y
+    pen.goto(x, y)
+
+    # Usado para desenhar com caneta
+    pen.pendown()
+
+    # Desenha o formato do trangulo
+    for i in range(3):
+        pen.fd(100)
+        pen.lt(120)
+        pen.fd(100)
+
+# Envia o cursor da posicao atual para o triangulo
 turtle.onscreenclick(triangle, 1)
 
 turtle.listen()
+
+# Segura a tela
 turtle.done()
